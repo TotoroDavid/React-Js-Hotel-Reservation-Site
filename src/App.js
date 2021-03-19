6d9f5e77d83d71c4ea3a6613f398c6396fa14192
TotoroDavid <davidtoro65@hotmail.com>
2021-03-19 12:11:23 -0500

    Add the FeaturedRooms and create Loading data  . . .

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ import Navbar from './components/Navbar'
 function App() {
   return (
     <>
-    <Navbar/>
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/rooms/" component={Rooms}/>
-      <Route exact path="/rooms/:slug" component={SingleRoom}/>
-      <Route component={Error} />
-    </Switch>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms/" component={Rooms}/>
+        <Route exact path="/rooms/:slug" component={SingleRoom}/>
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
